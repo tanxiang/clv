8536277a8ebdede409a8c73b5b2c39685c6698d4
Tan Xiang <wqazsedc@gmail.com>
2013-03-17 15:43:43 +0800

    istream itr

--- clvfile.cc
+++ clvfile.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <iterator>
 #include "clvfile.h"
 
 using namespace std;
 
 int main(int argc,char* argv[])
 {
-
+	istream_iterator<string> file_itr(cin);
+	file_text text(file_itr,istream_iterator<string>());
 };
