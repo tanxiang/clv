7dc8c2af8a5fbe56e9e26c47f8138b13d8a4c34d
Tan Xiang <wqazsedc@gmail.com>
2013-03-18 14:13:14 +0800

    it by line

--- clvfile.cc
+++ clvfile.cc
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <iterator>
+#include <algorithm>
 #include "clvfile.h"
 
 using namespace std;
 
 int main(int argc,char* argv[])
 {
-	//istream_iterator<string> file_itr(cin);
-	file_text text{istream_iterator<string>{cin},istream_iterator<string>{}};
-};
+	ios::sync_with_stdio(false);
+	if(argc!=2)
+		return 0;
+	ifstream ifs_in{argv[1]};
+	file_text text{istream_iterator<line>{ifs_in},istream_iterator<line>{}};
+	copy(text.begin(),text.end(),ostream_iterator<line>{cout,"\n"});
+}
--- clvfile.h
+++ clvfile.h
@@ -2,5 +2,12 @@
 #include <vector>
 #include <list>
 
-typedef std::vector<std::string> file_text;
+class line : public std::string {};
+static inline std::istream &operator>>(std::istream &is, line &l)
+{
+    std::getline(is, l);
+    return is;
+}
+
+typedef std::vector<line> file_text;
 typedef std::list<file_text> files_text;
