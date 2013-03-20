87ca5625c3464d7c9043829afd9268dd7158b6fb
Tan Xiang <wqazsedc@gmail.com>
2013-03-20 13:02:15 +0800

    clang port test

--- clangparse.cc
+++ clangparse.cc
@@ -1,3 +1,5 @@
+#define __STDC_LIMIT_MACROS
+#define __STDC_CONSTANT_MACROS
 #include <clang/Frontend/FrontendActions.h>
 #include <clang/Frontend/ASTConsumers.h>
 #include <clang/AST/RecursiveASTVisitor.h>
@@ -6,5 +8,14 @@
 
 using namespace clang::tooling;
 
-CommonOptionsParser OptionsParser(argc, argv);
+//CommonOptionsParser OptionsParser(argc, argv);
 
+int main(int argc,const char** argv){
+	//CommonOptionsParser OptionsParser(argc, argv);
+	//ClangTool Tool(OptionsParser.GetCompilations(),OptionsParser.GetSourcePathList());
+	//return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>());
+	//runToolOnCode( );
+	
+	//vs_args.push_back("-std=c++11");
+	runToolOnCodeWithArgs(new clang::SyntaxOnlyAction,"class X {};",std::vector<std::string> {1,"-std=c++11"});
+}
