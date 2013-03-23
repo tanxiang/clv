2aa29cfdbd86d601702d201809d4d423addca7df
Tan Xiang <wqazsedc@gmail.com>
2013-03-23 18:09:22 +0800

    action def

--- clangparse.cc
+++ clangparse.cc
@@ -6,14 +6,29 @@
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Tooling.h>
 
+#include "clang/Frontend/CompilerInstance.h"
+#include <iostream>
 using namespace clang::tooling;
-
+using namespace clang;
 //CommonOptionsParser OptionsParser(argc, argv);
+class ClpAction:public ASTFrontendAction{
+protected:
+	virtual clang::ASTConsumer *CreateASTConsumer(CompilerInstance &CI, llvm::StringRef InFile); 
+public:
+	virtual bool hasCodeCompletionSupport() const { return true; }
+};
+
+ASTConsumer* ClpAction::CreateASTConsumer(CompilerInstance &CI, llvm::StringRef InFile){
+	std::cout<<"cASTConsumer"<<std::endl;
+	return nullptr;
+}
 
 int main(int argc,const char** argv){
 	//CommonOptionsParser OptionsParser(argc, argv);
 	//ClangTool Tool(OptionsParser.GetCompilations(),OptionsParser.GetSourcePathList());
 	//return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>());
 	
-	runToolOnCodeWithArgs(new clang::SyntaxOnlyAction," class X {public:\n int in;};X x1{};\nint main(){ x1.in=0;}",std::vector<std::string> {1,"-std=c++11"});
+	runToolOnCodeWithArgs(new ClpAction,
+	" class X {public:\n int in;};X x1{};\nint main(){ x1.in=0;}",
+	std::vector<std::string> {"-std=c++11"});
 }
