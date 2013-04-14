c41d162c191fbfe2fc6d3af8eccff263c3e7420e
Tan Xiang <wqazsedc@gmail.com>
2013-04-14 17:03:29 +0800

    修改：      build.ninja 修改：      view.cc

--- view.cc
+++ view.cc
@@ -3,6 +3,7 @@
 #include "view.h"
 #include "SkData.h"
 #include "SkCanvas.h"
+#include "SkDevice.h"
 #include "SkGraphics.h"
 #include "SkString.h"
 #include "SkTouchGesture.h"
@@ -12,6 +13,13 @@
 
 //#include "gm.h"
 #include <memory>
+
+
+#include "gl/GrGLInterface.h"
+#include "gl/GrGLUtil.h"
+#include "GrRenderTarget.h"
+#include "GrContext.h"
+#include "SkGpuDevice.h"
 
 static const char gUpdateWindowTitleEvtName[] = "SkUpdateWindowTitle";
 
@@ -53,6 +61,11 @@ private:
 	//SkScalar fZoomLevel;
 	//SkScalar fZoomScale;
 
+	GrContext*              fCurContext;
+	const GrGLInterface*    fCurIntf;
+	GrRenderTarget*         fCurRenderTarget;
+	int fMSAASampleCount;
+
 	typedef SkOSWindow INHERITED;
 };
 
@@ -83,39 +96,62 @@ static void inline postEventToSink(SkEvent* evt, SkEventSink* sink) {
 ClvWindow::ClvWindow(void* hwnd):INHERITED{hwnd},hwnd(hwnd){
 	cout<<__FUNCTION__<<endl;
 
-	//register canvas here?
-
-	auto fTypeface = SkTypeface::CreateFromTypeface(NULL, SkTypeface::kBold);
-
-	//load view
-	SkView::F2BIter iter(this);
-	SkView* prev = iter.next();
-	if (prev) {
-		cout<<"get prev\n"<<endl;
-		prev->detachFromParent();
+	AttachmentInfo attachmentInfo;
+	bool result = attach(kNativeGL_BackEndType, 2, &attachmentInfo);
+	if(!result){
+		cerr<<"3D Attch err"<<endl;
+		//canvas = INHERITED::createCanvas();
+		//return canvas;
 	}
-	//attachChildToFront(new ClvView{})->unref();
+		cerr<<"try 3D Attch"<<endl;
+	fCurIntf = GrGLCreateNativeInterface();
+		cerr<<"try 3D Attch"<<endl;
+	fCurContext = GrContext::Create(kOpenGL_GrBackend, (GrBackendContext) fCurIntf);
+		cerr<<"try 3D Create"<<endl;
+	if (NULL == fCurContext || NULL == fCurIntf) {
+		cerr<<"3D Create err"<<endl;
+		detach();
+	}
+	GrBackendRenderTargetDesc desc;
+	desc.fWidth = SkScalarRound(width());
+	desc.fHeight = SkScalarRound(height());
+	desc.fConfig = kSkia8888_GrPixelConfig;
+	desc.fOrigin = kBottomLeft_GrSurfaceOrigin;
+	desc.fSampleCnt = attachmentInfo.fSampleCount;
+	desc.fStencilBits = attachmentInfo.fStencilBits;
+	GrGLint buffer;
+	GR_GL_GetIntegerv(fCurIntf, GR_GL_FRAMEBUFFER_BINDING, &buffer);
+	desc.fRenderTargetHandle = buffer;
+	fCurRenderTarget = fCurContext->wrapBackendRenderTarget(desc);
+	fCurContext->setRenderTarget(fCurRenderTarget);
+	//auto fTypeface = SkTypeface::CreateFromTypeface(NULL, SkTypeface::kBold);
+	//load view
+	//SkView::F2BIter iter(this);
+	//SkView* prev = iter.next();
+	//if (prev) {
+	//	cout<<"get prev\n"<<endl;
+	//	prev->detachFromParent();
+	//}
+
 	//setConfig(SkBitmap::kRGB_565_Config);
 	setConfig(SkBitmap::kARGB_8888_Config);
-	//loadView();
-	
+	setVisibleP(true);
+	//setClipToBounds(false);
+	//attachChildToFront(new ClvView{})->unref();	
 }
 
 SkCanvas* ClvWindow::createCanvas() {
-	cout<<__FUNCTION__<<endl;
-	SkCanvas* canvas = NULL;
-        //if (fDevManager) {
-        //    canvas = fDevManager->createCanvas(fDeviceType, this);
-        //}
-        //if (NULL == canvas) {
-        //    canvas = this->INHERITED::createCanvas();
-        //}
-	canvas = INHERITED::createCanvas();
-	return canvas;
+	cout<<__FUNCTION__<<endl;	//return INHERITED::createCanvas();
+	SkAutoTUnref<SkDevice> device{new SkGpuDevice{fCurContext, fCurRenderTarget}};
+		cerr<<"try 3D dev"<<endl;
+
+	return new SkCanvas{device};
+
 }
 
 void ClvWindow::draw(SkCanvas* canvas){
 	cout<<__FUNCTION__<<endl;
+		canvas->drawColor(0xFFFF8080);
 }
 
 bool ClvWindow::onEvent(const SkEvent& evt){
