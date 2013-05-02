b647b49dc8101689b3c65497feb931280dd04d17
Tan Xiang <wqazsedc@gmail.com>
2013-05-02 17:53:15 +0800

    clean

--- clthread.cc
+++ clthread.cc
@@ -1,9 +1,6 @@
-#include <iostream>
-//#include <fstream>
-#include <iterator>
-#include <algorithm>
+#include <thread>
+
 #include "filemap.h"
-//#include "filemap.cc"
 #include "clthread.h"
 #include "clangparse.h"
 
@@ -14,13 +11,8 @@ int clmain(int argc,char* argv[])
 	ios::sync_with_stdio(false);
 	if(argc!=2)
 		return 0;
-	//ifstream ifs_in{argv[1]};
-	//FileText text{istream_iterator<Line>{ifs_in},istream_iterator<Line>{}};
-	//runToolOnCodeWithArgs(new ClpAction,static_cast<vector<string> >(text),
-	//std::vector<std::string> {"-std=c++11"});
-	//copy(text.begin(),text.end(),ostream_iterator<Line>{cout,"\n"});
 	FileMap<MBLineRef<CharRef> > Map{argv[1]};
-	//cout<<static_cast<char*>(Map.Get());
+
 	ClpInvocation Invocation{vector<string>{"-std=c++11","-c"},new ClpAction{}};
 	Invocation.RunCode(static_cast<char*>(Map.Get()),Map.Length());
 }
--- clthread.h
+++ clthread.h
@@ -1,14 +1,2 @@
-#include <string>
-#include <vector>
-#include <list>
 
-class Line : public std::string {};
-static inline std::istream &operator>>(std::istream &is, Line &l)
-{
-    std::getline(is,l);
-    return is;
-}
-
-typedef std::vector<Line> FileText;
-typedef std::list<FileText> FilesText;
 
