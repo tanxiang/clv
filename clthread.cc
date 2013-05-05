246b9322b75dac77aeaaf64c9b6c85dd784a92d6
Tan Xiang <wqazsedc@gmail.com>
2013-05-05 14:56:50 +0800

    test std thread code

--- clthread.cc
+++ clthread.cc
@@ -4,17 +4,25 @@
 #include "clangparse.h"
 
 using namespace std;
-thread clangt;
+thread gt;
+
 
 void parse_thread(void* P,int Len)
 {
-	thread clangt{[&]{
+	//if(gt.joinable())
+	//	cout<<"canjoin";
+	//thread clangt{cl,P,Len};
+	thread clangt{[=]{
 		ClpInvocation Invocation{vector<string>{"-std=c++11","-c"},new ClpAction{}};
 		Invocation.RunCode(static_cast<char*>(P),Len);
 	}};
-	//cout<<"pppppp"<<endl;
+	gt=move(clangt);
+	//if(gt.joinable())
+	//	cout<<"canjoin2\n";
+	gt.detach();
+
 	//clangt.detach();
-	cout << "Hello, Thread!\n";
-	this_thread::sleep_for(std::chrono::seconds(6));
-	clangt.detach();
+	//cout << "Hello, Thread!\n";
+	//this_thread::sleep_for(std::chrono::seconds(6));
+	//clangt.detach();
 }
