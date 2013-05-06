0db162a6ed4323817f264165ce957bdd3db76da7
Tan Xiang <wqazsedc@gmail.com>
2013-05-06 13:12:47 +0800

    clang thread class

--- clthread.cc
+++ clthread.cc
@@ -5,25 +5,20 @@
 
 using namespace std;
 //thread gt;
+void* ClvCompile::PchCode(const char* CodeName,void* P,int Len){
+	return nullptr;
+}
 
-
-void parse_thread(const char* CodeName,void* P,int Len)
-{
-	//if(gt.joinable())
-	//	cout<<"canjoin";
-	//thread clangt{cl,P,Len};
-	cout<<CodeName<<endl;
-	thread clangt{[=]{
+bool ClvCompile::ParseCode(const char* CodeName,void* P,int Len,void* Pch){
+	CompileThread = thread{[=]{
 		ClpInvocation Invocation{new ClpAction{}};
 		Invocation.RunCode(CodeName,static_cast<char*>(P),Len,vector<string>{"-std=c++11","-c"});
 	}};
-	//gt=move(clangt);
-	//if(gt.joinable())
-	//	cout<<"canjoin2\n";
-	//gt.detach();
-
-	clangt.detach();
-	//cout << "Hello, Thread!\n";
-	//this_thread::sleep_for(std::chrono::seconds(6));
-	//clangt.detach();
+	CompileThread.detach();
+	return true;
 }
+
+bool ClvCompile::SearchAST(){
+	return true;
+}
+ClvCompile clast;
--- clthread.h
+++ clthread.h
@@ -1,13 +1,21 @@
 #include <future>
-class Compile{
+class ClvCompile{
 	std::thread CompileThread;
 	std::thread SearchThread;
 public:
 	void* PchCode(const char* CodeName,void* P,int Len);
 	bool ParseCode(const char* CodeName,void* P,int Len,void* Pch=nullptr);
-	bool ParseCodeError();
-	bool Ready();
+	bool ParseCodeError(){
+		return true;
+	}
+	bool Ready(){
+		return true;
+	}
 	bool SearchAST();
 };
 
-void parse_thread(const char* CodeName,void* P,int Len);
+extern ClvCompile clast;
+inline void parse_thread(const char* CodeName,void* P,int Len)
+{
+	clast.ParseCode(CodeName,P,Len);
+}
