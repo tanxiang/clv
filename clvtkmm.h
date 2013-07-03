6643fa9668b3632e0078e56dd21a577b349069b0
Tan Xiang <wqazsedc@gmail.com>
2013-07-03 16:45:36 +0800

    delete PrintOperation

--- clvtkmm.h
+++ clvtkmm.h
@@ -19,27 +19,6 @@ private:
 	void draw_text(const Cairo::RefPtr<Cairo::Context>& cr, int rectangle_width, int rectangle_height);
 };
 
-//We derive our own class from PrintOperation,
-//so we can put the actual print implementation here.
-/*
-class PrintFormOperation : public Gtk::PrintOperation
-{
-public:
-	static Glib::RefPtr<PrintFormOperation> create();
-	virtual ~PrintFormOperation(){};
-	void set_name(const Glib::ustring& name) { m_Name = name; }
-	void set_comments(const Glib::ustring& comments) { m_Comments = comments; }
-protected:
-	PrintFormOperation(){};
-	//PrintOperation default signal handler overrides:
-	void on_begin_print(const Glib::RefPtr<Gtk::PrintContext>& context);
-	void on_draw_page(const Glib::RefPtr<Gtk::PrintContext>& context, int page_nr);
-	Glib::ustring m_Name;
-	Glib::ustring m_Comments;
-	Glib::RefPtr<Pango::Layout> m_refLayout;
-	std::vector<int> m_PageBreaks; // line numbers where a page break occurs
-};
-*/
 
 class clvtkwindow : public Gtk::Window
 {
--- gtk_window.cc
+++ gtk_window.cc
@@ -3,7 +3,6 @@
 
 int main(int argc, char *argv[]){
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-	//Gtk::Main kit(argc, argv);
 	clvtkwindow window;
 	window.set_title("Drawing text example");
 	ClvArea area;
