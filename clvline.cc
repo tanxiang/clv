eb8e6f8d8b9689f03cece89baf7907f7c717939b
Tan Xiang <wqazsedc@gmail.com>
2013-09-01 00:35:57 +0800

    draw test

--- clvline.cc
+++ clvline.cc
@@ -2,14 +2,14 @@
 
 int _cairo_utf8_get_char_validated (const char *p,uint32_t *unicode);
 
-cairo_status_t _cairo_utf8_to_ucs4 (const char *str,int len, uint32_t **result, int	*items_written);
+//cairo_status_t _cairo_utf8_to_ucs4 (const char *str,int len, uint32_t **result, int	*items_written);
 
 int _cairo_ucs4_to_utf8 (uint32_t unicode,char *utf8);
 
 void line::sync_glyphs(unsigned int g_index){
 	if(g_index>glyphs.size())
 		g_index=0;
-	unsigned int c_index=glyphs_index[g_index];
+	size_t c_index=glyphs_index[g_index];
 	uint32_t ucs4;
 	while(size()>c_index){
 		c_index += _cairo_utf8_get_char_validated(&(this->c_str()[c_index]),&ucs4);	
