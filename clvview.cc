b8374f6c7c678a954078594a0bca60adf7d0340d
Tan Xiang <wqazsedc@gmail.com>
2013-09-24 16:07:23 +0800

    im test

--- clvview.cc
+++ clvview.cc
@@ -13,15 +13,28 @@ ClvFileArea::ClvFileArea(unorder_tree<line> &file):
 	gtk_widget_add_events (Widget::gobj() , GDK_BUTTON_PRESS_MASK );
 	gtk_widget_add_events (Widget::gobj() , GDK_BUTTON_RELEASE_MASK );
 	im_context = gtk_im_multicontext_new();
-	//gtk_im_context_set_client_window(im_context,get_window()->gobj());
-	//gtk_im_context_set_client_window(im_context,nullptr);
-	//Gdk::Window::create(get_parent_window(), <#GdkWindowAttr *attributes#>, <#int attributes_mask#>)
 	
 	g_signal_connect (im_context, "commit", G_CALLBACK (&ClvFileArea::commit_proxy), this);
 	g_signal_connect (im_context, "preedit-changed",G_CALLBACK (&ClvFileArea::commit_proxy), this);
 	g_signal_connect (im_context, "retrieve-surrounding",G_CALLBACK (&ClvFileArea::commit_proxy), this);
 	g_signal_connect (im_context, "delete-surrounding",G_CALLBACK (&ClvFileArea::commit_proxy), this);
-	
+	GdkWindowAttr attributes;
+	attributes.window_type = GDK_WINDOW_CHILD;
+	attributes.wclass = GDK_INPUT_ONLY;
+	attributes.event_mask = get_events();
+	attributes.event_mask |= (GDK_BUTTON_PRESS_MASK |
+		GDK_BUTTON_RELEASE_MASK |
+		GDK_BUTTON1_MOTION_MASK |
+		GDK_BUTTON3_MOTION_MASK |
+		GDK_POINTER_MOTION_HINT_MASK |
+		GDK_POINTER_MOTION_MASK |
+		GDK_ENTER_NOTIFY_MASK |
+		GDK_LEAVE_NOTIFY_MASK);
+	//gtk_im_context_set_client_window(im_context,get_window()->gobj());
+	//gtk_im_context_set_client_window(im_context,nullptr);
+	auto client_window = Gdk::Window::create(get_parent_window(), &attributes, GDK_WA_X | GDK_WA_Y);
+	register_window(client_window);
+	gtk_im_context_set_client_window(im_context,client_window->gobj());
 	property_can_focus() = true;
 	auto style = get_style_context();
 	style->context_save();
@@ -88,8 +101,10 @@ bool ClvFileArea::on_button_press_event(GdkEventButton* event){
 	if(gtk_widget_has_focus(Widget::gobj())){
 		//std::cout<<"focus"<<std::endl;
 	}
-	else
+	else{
 		grab_focus();
+		gtk_im_context_focus_in(im_context);
+	}
 	return true;
 }
 
