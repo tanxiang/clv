b44c1d1eb77902df55fce79f32f57f09a815ae1f
Tan Xiang <wqazsedc@gmail.com>
2013-09-28 00:11:17 +0800

    im test

--- clvview.cc
+++ clvview.cc
@@ -14,31 +14,10 @@ ClvFileArea::ClvFileArea(unorder_tree<line> &file):
 	gtk_widget_add_events (Widget::gobj() , GDK_BUTTON_RELEASE_MASK );
 	im_context = gtk_im_multicontext_new();
 	
-	g_signal_connect (im_context, "commit", G_CALLBACK (&ClvFileArea::commit_proxy), this);
-	g_signal_connect (im_context, "preedit-changed",G_CALLBACK (&ClvFileArea::commit_proxy), this);
-	g_signal_connect (im_context, "retrieve-surrounding",G_CALLBACK (&ClvFileArea::commit_proxy), this);
-	g_signal_connect (im_context, "delete-surrounding",G_CALLBACK (&ClvFileArea::commit_proxy), this);
-	GdkWindowAttr attributes;
-	attributes.x = 0;
-	attributes.y = 0;
-	attributes.width = 1;
-	attributes.height = 1;
-	attributes.window_type = GDK_WINDOW_CHILD;
-	attributes.wclass = GDK_INPUT_ONLY;
-	attributes.event_mask = get_events();
-	attributes.event_mask |= (GDK_BUTTON_PRESS_MASK |
-		GDK_BUTTON_RELEASE_MASK |
-		GDK_BUTTON1_MOTION_MASK |
-		GDK_BUTTON3_MOTION_MASK |
-		GDK_POINTER_MOTION_HINT_MASK |
-		GDK_POINTER_MOTION_MASK |
-		GDK_ENTER_NOTIFY_MASK |
-		GDK_LEAVE_NOTIFY_MASK);
-	//gtk_im_context_set_client_window(im_context,get_window()->gobj());
-	//gtk_im_context_set_client_window(im_context,nullptr);
-	auto client_window = Gdk::Window::create(get_parent_window(), &attributes, GDK_WA_X | GDK_WA_Y);
-	register_window(client_window);
-	gtk_im_context_set_client_window(im_context,client_window->gobj());
+	g_signal_connect (im_context, "commit", G_CALLBACK (&ClvFileArea::im_commit_proxy), this);
+	g_signal_connect (im_context, "preedit-changed",G_CALLBACK (&ClvFileArea::preedit_changed_proxy), this);
+	g_signal_connect (im_context, "retrieve-surrounding",G_CALLBACK (&ClvFileArea::retrieve_surrounding_proxy), this);
+	g_signal_connect (im_context, "delete-surrounding",G_CALLBACK (&ClvFileArea::delete_surrounding_proxy), this);
 	property_can_focus() = true;
 	auto style = get_style_context();
 	style->context_save();
@@ -51,6 +30,33 @@ ClvFileArea::ClvFileArea(unorder_tree<line> &file):
 }
 
 #include <iostream>
+
+void ClvFileArea::on_realize(){
+	GdkWindowAttr attributes;
+	auto allocation = get_allocation();
+	attributes.x = allocation.get_x();
+	attributes.y = allocation.get_y();
+	attributes.width =1;// allocation.get_width();
+	attributes.height =1;// allocation.get_height();
+	std::cerr<<attributes.x<<":"<<attributes.y<<":"<<attributes.width<<":"<<attributes.height<<"\n";
+	attributes.window_type = GDK_WINDOW_CHILD;
+	attributes.wclass = GDK_INPUT_ONLY;
+	attributes.event_mask = get_events();
+	attributes.event_mask |= (GDK_BUTTON_PRESS_MASK |
+		GDK_BUTTON_RELEASE_MASK |
+		GDK_BUTTON1_MOTION_MASK |
+		GDK_BUTTON3_MOTION_MASK |
+		GDK_POINTER_MOTION_HINT_MASK |
+		GDK_POINTER_MOTION_MASK |
+		GDK_ENTER_NOTIFY_MASK |
+		GDK_LEAVE_NOTIFY_MASK);
+	auto client_window = Gdk::Window::create(get_parent_window(), &attributes, GDK_WA_X | GDK_WA_Y );
+	//client_window->show();
+	register_window(client_window);
+	client_window->lower();
+	gtk_im_context_set_client_window(im_context,client_window->gobj());
+	Gtk::DrawingArea::on_realize();
+}
 
 void ClvFileArea::draw(const Cairo::RefPtr<Cairo::Context>& cr,const Cairo::Rectangle &rect){
 	cr->save();//need RAII mode restore
@@ -68,8 +74,6 @@ void ClvFileArea::draw(const Cairo::RefPtr<Cairo::Context>& cr,const Cairo::Rect
 	cr->clip();
 	cr->restore();
 }
-
-
 
 bool ClvFileArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
 	cr->save();
@@ -96,18 +100,44 @@ bool ClvFileArea::on_key_release_event(GdkEventKey *event){
 	return true;
 }
 
+void ClvFileArea::im_commit_proxy(GtkIMContext *context,const gchar  *str,ClvFileArea* pobj){
+	pobj->im_commit(context,str);
+}
+
+void ClvFileArea::im_commit(GtkIMContext *context,const gchar *str){
+	std::cerr<<"commit"<<str<<'\n';
+}
+
+void ClvFileArea::preedit_changed_proxy(GtkIMContext *context,ClvFileArea *pobj){
+	pobj->preedit_changed(context);
+}
+
+void ClvFileArea::preedit_changed(GtkIMContext *context){
+	std::cerr<<"preedit_changed"<<'\n';
+}
+
+void ClvFileArea::retrieve_surrounding_proxy(GtkIMContext *context,ClvFileArea *pobj){
+	std::cerr<<"retrieve_surrounding_proxy"<<'\n'; 
+}
+
+void ClvFileArea::delete_surrounding_proxy(GtkIMContext *context,gint offset,gint n_chars,ClvFileArea *pobj){
+	std::cerr<<"delete_surrounding_proxy"<<'\n'; 
+}
+
+bool ClvFileArea::on_focus_in_event(GdkEventFocus* event){
+	gtk_im_context_focus_in(im_context);
+	return false;
+}
+
 bool ClvFileArea::on_button_press_event(GdkEventButton* event){
-	std::cerr<<"pat"<<event->x<<","<<event->y<<"\n";
-	auto line_itr=file_context.get_form_fill(event->y);
-	if(line_itr!=file_context.end()){
-		line_itr->x_to_index(event->x);
-	}
-	if(gtk_widget_has_focus(Widget::gobj())){
-		//std::cout<<"focus"<<std::endl;
-	}
-	else{
+	if(!gtk_widget_has_focus(Widget::gobj())){
 		grab_focus();
-		gtk_im_context_focus_in(im_context);
+	}
+	if(event->button == GDK_BUTTON_PRIMARY){
+		auto line_itr=file_context.get_form_fill(event->y);
+		if(line_itr!=file_context.end()){
+			line_itr->x_to_index(event->x);
+		}
 	}
 	return true;
 }
--- clvview.h
+++ clvview.h
@@ -24,20 +24,32 @@ class ClvFileArea: public Gtk::DrawingArea{
  	bool button_pressed (GdkEventButton * ev);
 public:
 	void draw(const Cairo::RefPtr<Cairo::Context>& cr,const Cairo::Rectangle &rect);
-	static void commit_proxy(GtkIMContext *context,const gchar  *str,ClvFileArea* pobj){
-	}
+
 	ClvFileArea(unorder_tree<line> &file);
 	virtual ~ClvFileArea(){
 		g_object_unref (im_context);
 	};
 	
 protected:
+
+	static void im_commit_proxy(GtkIMContext *context,const gchar *str,ClvFileArea* pobj);
+	void im_commit(GtkIMContext *context,const gchar *str);
+
+	static void preedit_changed_proxy(GtkIMContext *context,ClvFileArea* pobj);
+	void preedit_changed(GtkIMContext *context);
+	
+	static void retrieve_surrounding_proxy(GtkIMContext *context,ClvFileArea* pobj);
+	
+	static void delete_surrounding_proxy(GtkIMContext *context,gint offset,gint n_chars,ClvFileArea* pobj);
+
+	bool on_focus_in_event(GdkEventFocus* event) override;
 	bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
 	bool on_key_press_event(GdkEventKey* event) override;
 	bool on_key_release_event(GdkEventKey* event) override;
 	bool on_button_press_event(GdkEventButton* event) override;
 	bool on_button_release_event(GdkEventButton* event) override;
 	//bool on_scroll_event(GdkEventScroll* event) override;
+	void on_realize() override;
 };
 
 class ClvThumArea : public Gtk::DrawingArea{
