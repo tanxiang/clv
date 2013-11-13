2b08b46517cca27a6808329736af347fddfa4554
Tan Xiang <wqazsedc@gmail.com>
2013-11-13 10:32:34 +0800

    cursor

---
+++ clvcontext.cc
@@ -0,0 +1,1 @@
+#include "clvcontext.h"
---
+++ clvcontext.h
@@ -0,0 +1,8 @@
+#ifndef _CLVCONTEXT_H
+#define _CLVCONTEXT_H
+#include "unorder_tree.h"
+template<typename T>
+class context :public unorder_tree<T>{
+	
+};
+#endif //_CLVCONTEXT_H
