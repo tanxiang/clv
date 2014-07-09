aca4d289411a771380a613309ec4bfae513c2cdc
Tan Xiang <wqazsedc@gmail.com>
2014-07-09 23:42:09 +0800

    fonts

--- source/clvhb.cc
+++ source/clvhb.cc
@@ -4,7 +4,14 @@
 namespace clv{
 
 hbfont::hbfont(){
+	blob=hb_blob_create();
+	hb_face_t *face = hb_face_create (blob, face_index);
+	  font = hb_font_create (face);
 
+  unsigned int upem = hb_face_get_upem (face);
+  hb_font_set_scale (font, upem, upem);
+	hb_face_destroy (face);
+	  hb_ot_font_set_funcs (font);
 }
 
 hbfont::~hbfont(){
