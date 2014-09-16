ac5dcebd0783ef276edd943d5bccd8b79448ae11
Tan Xiang <wqazsedc@gmail.com>
2014-09-16 13:57:21 +0800

    hb shape if

--- include/clvline.h
+++ include/clvline.h
@@ -40,8 +40,8 @@ public:
 		//std::cerr<<"line free\n";
 	//}
 	template<typename T>
-	line(T& refline){
-		std::string{refline.begin(),refline.end()};
+	line(T& refline):std::string{refline.begin(),refline.end()}{
+		
 	}
 
 	fill_t get_fill(){
