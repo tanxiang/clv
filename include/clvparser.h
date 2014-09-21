8a0a58d25dbb6ea0c12cbc04e395e19a91fc928f
Tan Xiang <wqazsedc@gmail.com>
2014-09-21 23:04:34 +0800

    pragma

--- include/clvparser.h
+++ include/clvparser.h
@@ -4,9 +4,18 @@
 #include <string>
 #include <thread>
 #include <memory>
+#include <condition_variable>
+
+namespace clp{
+class Invocation;
+}
+
 namespace clv{
 class Parser{
 	std::thread parse_thread;
+	std::unique_ptr<clp::Invocation> invocation;
+	std::condition_variable CondReady;
+	std::condition_variable CondSearch;
 protected:
 	std::shared_ptr<std::vector<std::string> > cmd;
 public:
--- source/clvparser.cc
+++ source/clvparser.cc
@@ -14,6 +14,7 @@ void Parser::complete_at_async(){
 	parse_thread=std::thread{
 		[=]{
 			std::shared_ptr<complete_table> complete_info{complete_at()};
+			//auto complete_info_sp = std::make_shared<complete_table>(complete_at());
 			//need notify editor ui, show
 			for(auto& line:*complete_info){
 				std::cerr<<std::get<0>(line)<<std::endl;
